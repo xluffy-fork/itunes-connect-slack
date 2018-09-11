19953fc84823d0a4d6061a128338ce4ed7fa4462
xluffy <xquang.foss@gmail.com>
2018-09-11 14:32:48 +0700

    Add rubocop, fix follow rubocop report

--- get-app-status.rb
+++ get-app-status.rb
@@ -2,37 +2,37 @@ require 'Spaceship'
 require 'json'
 
 # Constants
-itc_username = ENV["ITC_USERNAME"]
-itc_password = ENV["ITC_PASSWORD"]
+itc_username = ENV['ITC_USERNAME']
+itc_password = ENV['ITC_PASSWORD']
 
-raise "missing itunes username or password" if (!itc_username || !itc_password)
+raise 'missing itunes username or password' if !itc_username || !itc_password
 
 Spaceship::Tunes.login(itc_username, itc_password)
 apps = Spaceship::Tunes::Application.all
 versions = apps.map do |app|
-  editVersionInfo = app.edit_version
-  liveVersionInfo = app.live_version
+  edit_version_info = app.edit_version
+  live_version_info = app.live_version
 
   version = {}
 
-  if editVersionInfo
-  	version[:editVersion] = {
-  		name: app.name,
-  		version: editVersionInfo.version,
-  		status: editVersionInfo.app_status,
-  		appId: app.apple_id,
-  		iconUrl: app.app_icon_preview_url
-  	}
+  if edit_version_info
+    version[:edit_version] = {
+      name: app.name,
+      version: edit_version_info.version,
+      status: edit_version_info.app_status,
+      appId: app.apple_id,
+      iconUrl: app.app_icon_preview_url
+    }
   end
 
-  if liveVersionInfo
-  	version[:liveVersion] = {
-  		name: app.name,
-  		version: liveVersionInfo.version,
-  		status: liveVersionInfo.app_status,
-  		appId: app.apple_id,
-  		iconUrl: app.app_icon_preview_url
-  	}
+  if live_version_info
+    version[:live_version] = {
+      name: app.name,
+      version: live_version_info.version,
+      status: live_version_info.app_status,
+      appId: app.apple_id,
+      iconUrl: app.app_icon_preview_url
+    }
   end
 
   version
