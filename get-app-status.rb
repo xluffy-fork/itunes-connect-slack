1f8b36df145201740c61c4e1a8141073b7874afe
hungnm2904 <hungnm2904@gmail.com>
2018-09-11 11:06:47 +0700

    Implement for multiple app status alert of one itune account

--- get-app-status.rb
+++ get-app-status.rb
@@ -2,40 +2,40 @@ require 'Spaceship'
 require 'json'
 
 # Constants
-itc_username = ENV["itc_username"]
-bundle_id = ENV["bundle_id"]
+itc_username = ENV["ITC_USERNAME"]
+itc_password = ENV["ITC_PASSWORD"]
 
-if (!itc_username || !bundle_id)
-	puts "did not find username and bundle id"
-	exit
-end
+raise "missing itunes username or password" if (!itc_username || !itc_password)
 
-Spaceship::Tunes.login(itc_username)
-app = Spaceship::Tunes::Application.find(bundle_id)
-editVersionInfo = app.edit_version
-liveVersionInfo = app.live_version
+Spaceship::Tunes.login(itc_username, itc_password)
+apps = Spaceship::Tunes::Application.all
+versions = apps.map do |app|
+  editVersionInfo = app.edit_version
+  liveVersionInfo = app.live_version
 
-# send app info to stdout as JSON
-versions = Hash.new
+  version = {}
 
-if editVersionInfo
-	versions["editVersion"] = {
-		"name" => app.name,
-		"version" => editVersionInfo.version,
-		"status" => editVersionInfo.app_status,
-		"appId" => app.apple_id,
-		"iconUrl" => app.app_icon_preview_url
-	}
-end
+  if editVersionInfo
+  	version[:editVersion] = {
+  		name: app.name,
+  		version: editVersionInfo.version,
+  		status: editVersionInfo.app_status,
+  		appId: app.apple_id,
+  		iconUrl: app.app_icon_preview_url
+  	}
+  end
 
-if liveVersionInfo
-	versions["liveVersion"] = {
-		"name" => app.name,
-		"version" => liveVersionInfo.version,
-		"status" => liveVersionInfo.app_status,
-		"appId" => app.apple_id,
-		"iconUrl" => app.app_icon_preview_url
-	}
+  if liveVersionInfo
+  	version[:liveVersion] = {
+  		name: app.name,
+  		version: liveVersionInfo.version,
+  		status: liveVersionInfo.app_status,
+  		appId: app.apple_id,
+  		iconUrl: app.app_icon_preview_url
+  	}
+  end
+
+  version
 end
 
 puts JSON.dump versions
--- poll-itc.js
+++ poll-itc.js
@@ -1,46 +1,60 @@
-var poster = require('./post-update.js');
-var dirty = require('dirty');
-var db = dirty('kvstore.db');
-var pollIntervalSeconds = 120
-var debug = false
+require("dotenv").config();
+const poster = require("./post-update.js");
+const dirty = require("dirty");
+const db = dirty(process.env.DB_NAME || "kvstore.db");
+const pollIntervalSeconds = process.env.POLL_INTERVAL_SECONDS || 120;
 
 function checkAppStatus() {
-	console.log("Fetching latest app status...")
+  console.log("Fetching latest app status...");
 
-	// invoke ruby script to grab latest app status
-	var exec = require("child_process").exec;
-	exec('ruby get-app-status.rb', function (err, stdout, stderr) {
-		if (stdout) {
-			// compare new app info with last one (from database)
-			console.log(stdout)
-			var versions = JSON.parse(stdout);
+  // invoke ruby script to grab latest app status
+  var exec = require("child_process").exec;
+  exec("ruby get-app-status.rb", function(err, stdout, stderr) {
+    if (stderr) {
+      return console.error(stderr);
+    }
+    if (err) {
+      return console.error(error);
+    }
+    if (stdout) {
+      // compare new app info with last one (from database)
+      console.log(stdout);
+      var versions = JSON.parse(stdout);
 
-			// use the live version if edit version is unavailable
-			var currentAppInfo = versions["editVersion"] ? versions["editVersion"] : versions["liveVersion"];
-			var lastAppInfo = db.get('appInfo');
+      versions.forEach(version => {
+        // use the live version if edit version is unavailable
+        var currentAppInfo = version["editVersion"]
+          ? version["editVersion"]
+          : version["liveVersion"];
+        var lastAppInfo = db.get(`app-info-${currentAppInfo.appId}`);
 
-			if (!lastAppInfo || lastAppInfo.status != currentAppInfo.status || debug) {
-	    		poster.slack(currentAppInfo, db.get('submissionStart'));
+        if (
+          !lastAppInfo ||
+          lastAppInfo.status != currentAppInfo.status
+        ) {
+          poster.slack(currentAppInfo, db.get(`submission-start-${currentAppInfo.appId}`));
 
-	    		// store submission start time
-		    	if (currentAppInfo.status == "Waiting For Review") {
-					db.set('submissionStart', new Date());
-		    	}
-	    	}
-	    	else if (currentAppInfo) {
-	    		console.log(`Current status \"${currentAppInfo.status}\" matches previous status`);
-	    	}
-	    	else {
-	    		console.log("Could not fetch app status");
-	    	}
+          // store submission start time
+          if (currentAppInfo.status == "Waiting For Review") {
+            db.set(`submission-start-${currentAppInfo.appId}`, new Date());
+          }
+        } else if (currentAppInfo) {
+          console.log(
+            `Current status "${
+              currentAppInfo.status
+            }" of "${currentAppInfo.name}" matches previous status`
+          );
+        } else {
+          console.log(`Could not fetch status of "${currentAppInfo.name}"`);
+        }
 
-	    	// store latest app info in database
-	    	db.set('appInfo', currentAppInfo);
-		}
-		else {
-			console.log("There was a problem fetching the status of the app!");
-		}
-	});
+        // store latest app info in database
+        db.set(`app-info-${currentAppInfo.appId}`, currentAppInfo);
+      });
+    } else {
+      console.error(`There was a problem fetching the status of "${currentAppInfo.name}"!`);
+    }
+  });
 }
 
 setInterval(checkAppStatus, pollIntervalSeconds * 1000);
